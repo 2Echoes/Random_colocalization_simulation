a24ace701ee26ef70f7eb59004f7bcb4c617700a
2Echoes <floric.slimani@live.fr>
2025-07-03 17:12:58 +0200

    wip

--- scripts/run_simulation.jl
+++ scripts/run_simulation.jl
@@ -14,21 +14,22 @@ using JLD2
 using CSV
 
 #Simulation Parameters
-simulation_number = 100000
+replicate_number = 50000 #simulation replicates
+sampling_number = 100 # i.e number of cell per experiment
 distance_threshold = 0
 shape = (1, 158,158)
 distributions_abundacy = [10,50,100,200,250,400,500]
 
 """
-Performs a random co-localization simulation and saves results at the output_path parameters with name 'colocalization_counts_table' which will be an Int array of size (simulation_number,distributions_number,distributions_number)
-Self co-localization are not corrected.
+Performs random co-localization simulations and saves results at the output_path parameters.
 """
 function main(
     output_path =               length(ARGS) >=1 && ARGS[1] !='_' ? ARGS[1] : pwd(),
-    simulation_number =         length(ARGS) >=2 && ARGS[2] !='_' ? ARGS[2] : simulation_number,
-    distance_threshold =        length(ARGS) >=3 && ARGS[3] !='_' ? ARGS[3] : distance_threshold,
-    shape =                     length(ARGS) >=4 && ARGS[4] !='_' ? ARGS[4] : shape,
-    distributions_abundacy =    length(ARGS) >=5 && ARGS[5] !='_' ? ARGS[5] : distributions_abundacy
+    replicate_number =          length(ARGS) >=2 && ARGS[2] !='_' ? ARGS[2] : replicate_number,
+    sampling_number =           length(ARGS) >=3 && ARGS[3] !='_' ? ARGS[3] : sampling_number,
+    distance_threshold =        length(ARGS) >=4 && ARGS[4] !='_' ? ARGS[4] : distance_threshold,
+    shape =                     length(ARGS) >=5 && ARGS[5] !='_' ? ARGS[5] : shape,
+    distributions_abundacy =    length(ARGS) >=6 && ARGS[6] !='_' ? ARGS[6] : distributions_abundacy
     )
     
     #Init variables
@@ -43,59 +44,48 @@ function main(
         )
         CSV.write(output_path*"/colocalization_simulation_parameters.csv", Sumup_df)
         
-    println("Starting $simulation_number simulations for $distributions_number distributions :")
+    println("Starting $replicate_number simulations for $distributions_number distributions with $sampling_number cells:")
     
     #Generate coordinates
-    print("Generating random coordinates :")
-    @time for (key,value) in distributions_abundacy_dict
-        coordinates = generate_coordinates(shape, value, simulation_number)
-        distributions_coordinates[key] = coordinates
-    end
+    replicates_colocalization_truth_table = Vector(undef,replicate_number)
+    replicates_colocalization_truth_table_index = Vector(undef,replicate_number)
 
-    #Test pair colocalization
-    println("Counting pair co-localization :")
-    colocalization_counts = Array{Int}(undef,simulation_number,distributions_number,distributions_number)
-    @showprogress for line in 1:distributions_number
-        for col in 1:distributions_number
-
-            colocalization_counts[:,line,col] = compute_colocalization_count_across_simulations(
-                                                distributions_coordinates[col],
-                                                distributions_coordinates[line],
-                                                distance_threshold
-                                                )
-        end
-    end
-
-    #Building truth table for all co-localization
-    println("Testing all spots for all distribututions :")
-    total_number_spots = sum(distributions_abundacy)
-    colocalization_truth_table = Array{Bool,3}(undef,simulation_number,total_number_spots,distributions_number)
-    colocalization_truth_table_index = vcat(
-        (fill(idx, distributions_abundacy[idx]) for idx in 1:distributions_number)...
-     ) # index for colocalization_counts_table whith consistant order and value representing which distribution the line of truth table belongs to.
+    @showprogress for replicate in 1:replicate_number
     
-    
-    ## Initializing index for loop 
-    spot_idx_inf = 1
-    spot_idx_sup = nothing
-    @showprogress for line in 1:distributions_number
-        if line != 1 
-            spot_idx_inf = spot_idx_sup + 1
+            for (key,value) in distributions_abundacy_dict
+            coordinates = generate_coordinates(shape, value, sampling_number)
+            distributions_coordinates[key] = coordinates
         end
-        spot_idx_sup = spot_idx_inf + distributions_abundacy[line] -1
-        for col in 1:distributions_number
-            colocalization_truth_table[:,spot_idx_inf : spot_idx_sup,col] = compute_colocalization_across_simulations(
-            distributions_coordinates[col],
-            distributions_coordinates[line],
-            distance_threshold
-            )
+
+        #Building truth table for all co-localization
+        total_number_spots = sum(distributions_abundacy)
+        colocalization_truth_table = Array{Bool,3}(undef,sampling_number,total_number_spots,distributions_number)
+        colocalization_truth_table_index = vcat(
+            (fill(idx, distributions_abundacy[idx]) for idx in 1:distributions_number)...
+         ) # index for colocalization_counts_table whith consistant order and value representing which distribution the line of truth table belongs to.
+
+
+        ## Initializing index for loop 
+        spot_idx_inf = 1
+        spot_idx_sup = nothing
+        for line in 1:distributions_number
+            if line != 1 
+                spot_idx_inf = spot_idx_sup + 1
+            end
+            spot_idx_sup = spot_idx_inf + distributions_abundacy[line] -1
+            for col in 1:distributions_number
+                colocalization_truth_table[:,spot_idx_inf : spot_idx_sup,col] = compute_colocalization_across_simulations(
+                distributions_coordinates[col],
+                distributions_coordinates[line],
+                distance_threshold
+                )
+            end
         end
+        replicates_colocalization_truth_table[replicate] = colocalization_truth_table
+        replicates_colocalization_truth_table_index[replicate] = colocalization_truth_table_index
     end
 
-
-    println("Saving results at $output_path")
-    @save output_path*"/colocalization_pairwise_counts.jld2" colocalization_counts
-    @save output_path*"/colocalization_truth_table.jld2" colocalization_truth_table colocalization_truth_table_index shape
+    @save output_path*"/colocalization_truth_table.jld2" replicates_colocalization_truth_table replicates_colocalization_truth_table_index shape
 
 end #end main
 
--- src/modelisation.jl
+++ src/modelisation.jl
@@ -22,7 +22,7 @@ q(abundancy:: Int, volume :: Int):: Float64 = (1-2*(1-1/volume)^abundancy + (1-2
 """
 The covariance of the occupancy of two different voxel by the same distribution i.
 """
-c(abundacy::Int, volume::Int)::Float64 = q(abundacy, volume) - p(abundacy,volume)^2
+c(abundancy::Int, volume::Int)::Float64 = q(abundancy, volume) - p(abundancy,volume)^2
 
 function self_colocalization_expectancy(abundancy::Int, volume::Int)::Float64 
     return abundancy * p(abundancy -1,volume)
@@ -36,20 +36,20 @@ function self_colocalization_std(abundancy::Int, volume::Int)::Float64
 end
 
 """
-Expectancy of co-localization of distribution 1 (ie abundancy1) with distribution2 (abundacy2)
+Expectancy of co-localization of distribution 1 (ie abundancy1) with distribution2 (abundancy2)
 """
-function colocalization_expectancy(abundacy1::Int, abundacy2::Int, volume::Int)::Float64
-    return abundacy1*p(abundacy2,volume)
+function colocalization_expectancy(abundancy1::Int, abundancy2::Int, volume::Int)::Float64
+    return abundancy1*p(abundancy2,volume)
 end
 
 """
-Standard deviation of co-localization of distribution 1 (ie abundancy1) with distribution2 (abundacy2)
+Standard deviation of co-localization of distribution 1 (ie abundancy1) with distribution2 (abundancy2)
 """
-function colocalization_std(abundacy1::Int, abundacy2::Int, volume::Int)
-    p2 = p(abundacy2,volume)
-    c2 = c(abundacy2,volume)
+function colocalization_std(abundancy1::Int, abundancy2::Int, volume::Int)
+    p2 = p(abundancy2,volume)
+    c2 = c(abundancy2,volume)
 
-    variance = abundacy1*((1-1/volume) * (p2*(1-p2) - c2) + c2)
+    variance = abundancy1*((1-1/volume) * (p2*(1-p2) - c2) + c2)
 
     return sqrt(variance)
 end
@@ -58,17 +58,17 @@ end
 Expectancy deviation of voxel co-occupancy with at least 1 element of all distributions (ie abundancies).
 """
 function Ncolocalization_expectancy(abundancies::Vector{Int}, volume::Int)::Float64
-    p_combination = prod([p(abundacy, volume) for abundancy in abundancies])
-    return v*p_combination
+    p_combination = prod([p(abundancy, volume) for abundancy in abundancies])
+    return volume*p_combination
 end
 
 """
 Standard deviation of voxel co-occupancy with at least 1 element of all distributions (ie abundancies).
 """
 function Ncolocalization_std(abundancies::Vector{Int}, volume::Int)::Float64
-    p_combination = prod([p(abundacy, volume) for abundancy in abundancies])
-    q_combination = prod([q(abundacy, volume) for abundancy in abundancies])
-    variance = v*p_combination*(1-p_combination) + v(v-1)*(q_combination - p_combination^2)
+    p_combination = prod([p(abundancy, volume) for abundancy in abundancies])
+    q_combination = prod([q(abundancy, volume) for abundancy in abundancies])
+    variance = volume*p_combination*(1-p_combination) + volume*(volume-1)*(q_combination - p_combination^2)
     
     return sqrt(variance)
 end
